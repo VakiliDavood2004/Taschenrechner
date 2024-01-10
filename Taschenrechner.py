a7ab17a75f728445228eab88f50b15ec60e170f9
VakiliDavood2004 <vakilidavood2004@gmail.com>
2024-01-10 12:00:00 +0330

    Projektstart: Grundstruktur des wissenschaftlichen Taschenrechners mit PyQt5 erstellt

---
+++ Taschenrechner.py
@@ -0,0 +1,20 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QGridLayout, QPushButton, QLineEdit, QTextEdit
+from PyQt5.QtGui import QFont, QKeySequence
+from PyQt5.QtCore import Qt
+import qdarkstyle
+
+class Calculator(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        
+        
+        
+        
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setStyleSheet(qdarkstyle.load_stylesheet())
+    calc = Calculator()
+    sys.exit(app.exec_())
